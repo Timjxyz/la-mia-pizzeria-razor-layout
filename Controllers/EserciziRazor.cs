164e22072045106298d42a44eec2fc5dd9a11835
Timothy Gaither <timjgaither@gmail.com>
2022-07-14 14:26:48 +0200

    primo push creazione esercizi razor

---
+++ Controllers/EserciziRazor.cs
@@ -0,0 +1,18 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class EserciziRazor : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
